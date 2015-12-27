04dfa337d6837026a5c41e7779419cdbf7d28ed8
root <saulas.romain@gmail.com>
2015-12-27 17:50:14 +0100

    api of tasks done

--- src/api/tasks/index.php
+++ src/api/tasks/index.php
@@ -6,8 +6,18 @@ $tasks=new Tasks();
 $params = json_decode(file_get_contents('php://input'),true);
 
 switch($params['action']) {
-	//--------------- GET_TASKS ------------------
+	//--------------- GET_TASKS -------------------------------
 	case 'get_tasks':
+		echo json_encode($tasks->get_tasks($_SESSION['login']));
+		break;
+	//--------------- MOVE_TASKS --------------
+	case 'move_task_todo_inprogress':
+		$tasks.move_task($_SESSION['login'],$params['source'],$params['title']);
+		echo json_encode($tasks->get_tasks($_SESSION['login']));
+		break;
+	//--------------- REMOVE_TASK -----------------------------
+	case 'remove_task':
+		$tasks.remove_task($_SESSION['login'],$params['source'],$params['title']);
 		echo json_encode($tasks->get_tasks($_SESSION['login']));
 		break;
 }
