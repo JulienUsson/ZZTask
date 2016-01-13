24b587253e966ff4a0c52f8408c3cbb826cb9bb0
JulienUsson <usson.julien@gmail.com>
2016-01-13 18:43:41 +0100

    Corrections des tests concernant les users

--- src/classes/authentification.php
+++ src/classes/authentification.php
@@ -57,7 +57,6 @@ class Authentification {
 		if(!isset($users[$login])) {
 			$users[$login]=$this->_password_hash($password);
 			$this->_save_users($users);
-			touch(__dir__."/../private/tasks/".$login.".json");
 			return true;
 		}
 		return false;
@@ -68,7 +67,6 @@ class Authentification {
 		if(isset($users[$login])) {
 			unset($users[$login]);
 			$this->_save_users($users);
-			unlink(__dir__."/../private/tasks/".$login.".json");
 			return true;
 		}
 		return false;
