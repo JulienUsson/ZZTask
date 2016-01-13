1eafb1bf782df1331b6ddfa5158989ac4cb151ea
JulienUsson <usson.julien@gmail.com>
2016-01-13 17:54:35 +0100

    Refonte de la classe Tasks

--- src/classes/tasks.php
+++ src/classes/tasks.php
@@ -1,48 +1,34 @@
 <?php
 
 class Tasks {
-	public function _construct() {}
-
-	public function get_tasks($login) {
-		$tasks=json_decode(file_get_contents(__dir__."/../private/tasks/".$login.".json"), true);
-		if(!$tasks)
-			$tasks=array(array(), array(), array());
+	public function get_tasks() {
+		$tasks=json_decode(file_get_contents(__dir__."/../private/tasks.json"), true);
 		return $tasks;
 	}
 
-	private function _save_tasks($login, $tasks) {
-		file_put_contents(__dir__."/../private/tasks/".$login.".json", json_encode($tasks));
-	}
-
-
-	public function add_task($login, $title, $description) {
-		$tasks=$this->get_tasks($login);
-		$tasks[0][]=array('title' => $title, 'description' => $description, 'user' => $login);
-		$this->_save_tasks($login, $tasks);
+	private function _save_tasks($tasks) {
+		file_put_contents(__dir__."/../private/tasks.json", json_encode($tasks));
 		return true;
 	}
 
-	public function remove_task($login, $source, $index) {
-		$tasks=$this->get_tasks($login);
-		if(isset($tasks[$source][$index])) {
-			unset($tasks[$source][$index]);
-			$this->_save_tasks($login, $tasks);
-			return true;
-		}
-		return false;
-	}
-
-	public function move_task($login, $source, $index) {
-		$tasks=$this->get_tasks($login);
-		if(isset($tasks[$source][$index])) {
-			printf("lol");
-			$tasks[$source+1][]=$tasks[$source][$index];
-		}
-		$this->remove_task($login, $source, $index);
-		$this->_save_tasks($login, $tasks);
+	public function add_task($title, $description, $user, $state=0) {
+		$tasks=$this->get_tasks();
+		$tasks[]=array('title' => $title, 'description' => $description, 'user' => $user, 'state' => $state);
+		$this->_save_tasks($tasks);
 		return true;
 	}
 
-	//public function edit_task($login, $source)
+	public function remove_task($id) {
+		$tasks=$this->get_tasks();
+		unset($tasks[$id]);
+		$this->_save_tasks($tasks);
+		return true;
+	}
+
+	public function edit_task($id, $title, $description, $user, $state) {
+		$this->remove_task($id);
+		$this->add_task($title, $description, $user, $state);
+		return true;
+	}
 };
 ?>
--- tests/tasksTests.php
+++ tests/tasksTests.php
@@ -6,47 +6,27 @@ include(__DIR__.'/../src/classes/tasks.php');
 class tasksTests extends PHPUnit_Framework_TestCase {
 	public function testAddTask()
     {
-		$auth=new Authentification();
-		$tasks=new Tasks();
-		$auth->add_user('test_user','test_password');
-		$tasks_t=$tasks->get_tasks('test_user');
-		$this->assertEquals(count($tasks_t[0]), 0);
-		$tasks->add_task('test_user', 'test_title', 'test_task');
-		$tasks_t=$tasks->get_tasks('test_user');
-		$this->assertEquals(count($tasks_t[0]), 1);
-		$this->assertEquals($tasks_t[0][0]['title'], 'test_title');
-		$this->assertEquals($tasks_t[0][0]['description'], 'test_task');
-		$this->assertEquals($tasks_t[0][0]['user'], 'test_user');
-		$auth->remove_user('test_user');
+			$t=new Tasks();
+			$tasks=$t->get_tasks();
+			$nb_tasks=count($tasks);
+			$t->add_task('titre', 'description', 'toto');
+			$tasks=$t->get_tasks();
+			$this->assertEquals(count($tasks), $nb_tasks+1);
+			$this->assertEquals($tasks[count($tasks)-1]['title'], 'titre');
+			$this->assertEquals($tasks[count($tasks)-1]['description'], 'description');
+			$this->assertEquals($tasks[count($tasks)-1]['user'], 'toto');
+			$t->remove_task($nb_tasks);
     }
 
     public function testRemoveTask()
     {
-		$auth=new Authentification();
-		$tasks=new Tasks();
-		$auth->add_user('test_user','test_password');
-		$tasks->add_task('test_user', 'test_title', 'test_task');
-		$tasks->remove_task('test_user', 0, 0);
-		$tasks_t=$tasks->get_tasks('test_user');
-		$this->assertArrayNotHasKey(0,$tasks_t[0]);
-		$auth->remove_user('test_user');
+			$t=new Tasks();
+			$tasks=$t->get_tasks();
+			$t->add_task('titre', 'description', 'toto');
+			$nb_tasks=count($tasks);
+			$t->remove_task($nb_tasks-1);
+			$tasks=$t->get_tasks();
+			$this->assertEquals(count($tasks), $nb_tasks-1);
     }
-
-	public function testMoveTask()
-	{
-		// $auth=new Authentification();
-		// $tasks=new Tasks();
-		// $auth->add_user('test_user','test_password');
-		// $tasks->add_task('test_user', 'test_title', 'test_task');
-		// $tasks->move_task('test_user',0,0);
-		// $tasks_t=$tasks->get_tasks('test_user');
-		// $this->assertEquals(count($tasks_t[0]), 0);
-		// $this->assertEquals('title', $tasks_t[1][0]['title']);
-		// $tasks->move_task('test_user',1,0);
-		// $tasks_t=$tasks->get_tasks('test_user');
-		// $this->assertEquals(count($tasks_t[1]), 1);
-		// $this->assertEquals('test_title', $tasks_t[2][0]['title']);
-		// $auth->remove_user('test_user');
-	}
 }
 ?>
