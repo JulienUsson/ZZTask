c68846847caeb57da1b132f7ef21119df2bebf08
JulienUsson <usson.julien@gmail.com>
2016-01-13 18:07:09 +0100

    test edit task

--- tests/tasksTests.php
+++ tests/tasksTests.php
@@ -14,6 +14,7 @@ class tasksTests extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($tasks[count($tasks)-1]['title'], 'titre');
 		$this->assertEquals($tasks[count($tasks)-1]['description'], 'description');
 		$this->assertEquals($tasks[count($tasks)-1]['user'], 'toto');
+		$this->assertEquals($tasks[count($tasks)-1]['state'], 0);
 		$t->remove_task(count($tasks)-1);
   }
 
@@ -26,5 +27,18 @@ class tasksTests extends PHPUnit_Framework_TestCase {
 		$tasks=$t->get_tasks();
 		$this->assertEquals(count($tasks), $nb_tasks-1);
   }
+
+	public function testEditTask() {
+		$t=new Tasks();
+		$t->add_task('titre', 'description', 'toto');
+		$tasks=$t->get_tasks();
+		$t->edit_task(count($tasks)-1, 'titre2', 'description2', 'toto2', 1);
+		$tasks=$t->get_tasks();
+		$this->assertEquals($tasks[count($tasks)-1]['title'], 'titre2');
+		$this->assertEquals($tasks[count($tasks)-1]['description'], 'description2');
+		$this->assertEquals($tasks[count($tasks)-1]['user'], 'toto2');
+		$this->assertEquals($tasks[count($tasks)-1]['state'], 1);
+		$t->remove_task(count($tasks)-1);
+	}
 }
 ?>
